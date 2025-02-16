8210406f85ebeacc88844fbd30ad05c3a738046f
devkush0999 <devesh2cse@gmail.com>
2025-02-16 15:19:10 +0530

    .

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
+import dotenv from "dotenv";
 
+import express from "express";
 
+import userRoutes from "./routes/user.js";
+import categoryRoutes from "./routes/category.js";
+import productRoutes from "./routes/product.js"
 
+dotenv.config();
 
-const start =async() =>{
+const app = express();
 
-}
+app.use(express.json());
 
+// routes
+app.use("/user", userRoutes);
+app.use("/category", categoryRoutes);
+app.use("/product", productRoutes)
 
+const start = async () => {
+  try {
+    app.listen({ port: 3000, host: "0.0.0.0" }, (err, addr) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("server started on http://localhost:3000");
+      }
+    });
+  } catch (error) {
+    console.log("Error start server", error);
+  }
+};
 start();
